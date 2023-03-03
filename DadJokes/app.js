c13e31ce9875f38fcca8abdbd1ca3e0f0f648456
PDochev <plamendochev0@gmail.com>
2023-03-03 08:32:35 +0000

    Dad Jokes 2

--- DadJokes/app.js
+++ DadJokes/app.js
@@ -1,25 +1,29 @@
-const jokes = document.querySelector("#jokes")
-const buttonAdd = document.querySelector("#add")
-const buttonDelete = document.querySelector("#delete")
+const jokes = document.querySelector("#jokes");
+const buttonAdd = document.querySelector("#add");
+const buttonDelete = document.querySelector("#delete");
+
+buttonDelete.disabled = true;
 
 const addNewJoke = async () => {
-  const jokeText = await getDadJoke()
-  const newLI = document.createElement("LI")
-  newLI.append(jokeText)
-  jokes.append(newLI)
+  const jokeText = await getDadJoke();
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+  buttonDelete.disabled = false;
   buttonDelete.addEventListener("click", function () {
-    jokes.removeChild(newLI)
-  })
-}
+    jokes.removeChild(newLI);
+    buttonDelete.disabled = true;
+  });
+};
 
 const getDadJoke = async () => {
   try {
-    const config = { headers: { Accept: "application/json" } }
-    const res = await axios.get("https://icanhazdadjoke.com/", config)
-    return res.data.joke
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
   } catch (e) {
-    return "NO JOKES AVAILABLE! SORRY :("
+    return "NO JOKES AVAILABLE! SORRY :(";
   }
-}
+};
 
-buttonAdd.addEventListener("click", addNewJoke)
+buttonAdd.addEventListener("click", addNewJoke);
